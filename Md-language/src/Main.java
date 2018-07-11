02a80a9b54e8e8b07714a08390b2c789813d0efc
gizemsungu <gizemsungu@gmail.com>
2018-07-11 23:29:02 +0300

    adding graph generation style but not completed.

--- Md-language/src/Main.java
+++ Md-language/src/Main.java
@@ -1,6 +1,17 @@
+import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.SingleGraph;
+
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String [] args){
+        Graph graph = new SingleGraph("Gizem");
+        TreeMap<String, String > attributes = new TreeMap<>();
+        attributes.put("density", "15");
+        attributes.put("Nodes", "10");
+        MdGraphGenerator generator = new MdGraphGenerator("Random", graph, attributes);
+        System.out.println(generator.generateGraph());
+        System.out.println(graph.getNodeCount());
     }
 }
