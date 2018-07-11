e25b789366d461acad07d4df60fefc42048bf008
yilmazedis <ylmazedis@gmail.com>
2018-07-11 08:32:36 +0000

    This is my backend module until to day. I will add;
     add instance
     remove instance
     retrieve instance
     retrieve dataset
     filter

---
+++ math_data.py
@@ -0,0 +1,77 @@
+import os
+from git import Repo
+
+
+class math_data:
+    __sha_list = []
+
+    def __get_sha(self, path):
+
+        master = Repo(path).head.reference
+
+        return master.commit.hexsha
+
+    def __create_file(self, filename,repo_content,commit_message):
+
+        path = '.'
+        repo = Repo.init(path).git
+        index = Repo.init(path).index
+
+        # fill in the file
+        f = open(filename, "w")
+        f.write(repo_content)
+        f.close()
+
+        # add repo
+        repo.add(filename)
+        index.commit(commit_message)
+
+        # get repo sha
+
+        self.__sha_list.append(self.__get_sha(path=path))
+
+    def initial_setup(self, datatype, content):
+
+        repos = ("raw", "semantics", "typeset", "context", "features", "index")
+        try:
+            os.makedirs(datatype)
+            os.chdir(datatype)
+            for repo in repos:
+                os.makedirs(repo)
+                os.chdir(repo)
+                self.__create_file("file1.txt", content[repo], content["commit"])
+                os.chdir("..")
+            os.chdir("..")
+            return self.__sha_list
+        except OSError as e:
+            raise ValueError('Cannot create! ', e.filename, ' is already exist')
+
+    def log_repo(self, datatype, repo):
+        r = Repo('graph/features').git
+        print(r.log(p=True))
+
+    def edit_repo(self, datatype, repo, sha, commit):
+
+        os.chdir(datatype)
+        os.chdir(repo)
+
+        os.open("file1.txt", os.O_CREAT)
+
+    def list_repo(self,datatype, repo, sha):
+
+        os.chdir(datatype)
+        os.chdir(repo)
+
+        actual_sha = self.__get_sha(path='.')
+
+        if actual_sha == sha:
+            f = open("file1.txt", "r")
+            repo_content = f.read()
+            f.close()
+            os.chdir("..")
+            os.chdir("..")
+            return repo_content
+        else:
+            os.chdir("..")
+            os.chdir("..")
+            return ""
