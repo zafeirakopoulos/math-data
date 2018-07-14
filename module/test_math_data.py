69e7f37543b6d4d8285d69a66954072d4cce234d
yilmazedis <ylmazedis@gmail.com>
2018-07-14 11:28:33 +0000

    Update test_math_data.py

--- module/test_math_data.py
+++ module/test_math_data.py
@@ -9,14 +9,17 @@ r = {
     "raw": "something...",
     "features": "something...",
     "semantics": "something...",
-    "context": "something...",
+    "context":  {"edge": {"1": 1, "2": 2},
+                 "vertex": [{"first": 2, "second": 1},
+                            {"first": 23, "second": 55}]},
     "typeset": "something...",
     "commit": "something..."
 }
 
 response = data.add_instance(r)
-
 print(response)
+
+
 
 
 
