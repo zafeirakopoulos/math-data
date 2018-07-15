ae770193938fa8221202dbf05ebd2474c926d589
yilmazedis <ylmazedis@gmail.com>
2018-07-15 20:51:28 +0000

    added update_instance function

--- module/math_data.py
+++ module/math_data.py
@@ -30,25 +30,47 @@ class math_data:
 
     def __create_file(self, content, path, commit_message):
 
-        repo = Repo.init(path).git
-        index = Repo.init(path).index
         max_number = self.__find_last_file_number(os.listdir(path))
         filename = "file" + str(max_number) + ".txt"
         file_path = os.path.join(path, filename)
 
+        # write file
         with open(file_path, 'w') as outfile:
             json.dump(content, outfile)
-        # fill in the file
-        #f = open(file_path, "w")
-        #f.write(content)
-        #f.close()
+
+        found = 0
+        for file in os.listdir(path):
+            if file == ".git":
+                found = 1
+                break
 
         # add repo
+        if found == 0:
+            repo = Repo.init(path).git
+            index = Repo(path).index
+        else:
+            repo = Repo(path).git
+            index = Repo(path).index
+
         repo.add(filename)
         index.commit(commit_message)
 
         # get repo sha
         self.__sha_list.append(self.__get_sha(path=path, filename=filename))
+
+    def __update_file(self, content, path, commit_message, filename):
+
+        repo = Repo(path).git
+        index = Repo(path).index
+
+        file_path = os.path.join(path, filename)
+
+        with open(file_path, 'w') as outfile:
+            json.dump(content, outfile)
+
+        # add repo
+        repo.add(filename)
+        index.commit(commit_message)
 
     def __initial_setup(self, data):
 
@@ -96,7 +118,7 @@ class math_data:
                         file_path = os.path.join(os.path.curdir, datatype, attribute, filename)
                         os.remove(file_path)
                         path = os.path.join(os.path.curdir, datatype, attribute)
-                        index = Repo.init(path).index
+                        index = Repo(path).index
                         index.commit("deleted repo")
                     self.__status = 1
                     break
@@ -110,11 +132,40 @@ class math_data:
 
         return response
 
-    def retrieve_instance(self,data):
+    def retrieve_instance(self, data):
         print("")
 
-    def update_instance(self,data):
-        print("")
+    def update_instance(self, data):
+
+        datatype = data["datatype"]
+
+        if os.path.exists(datatype):
+
+            input_sha = data["sha"]
+
+            dir_index = os.path.join(os.path.curdir, datatype, "index")
+
+            for filename in os.listdir(dir_index):
+
+                if filename == ".git":
+                    continue
+
+                if input_sha == self.__get_sha(dir_index, filename):
+                    for attribute in self.__attributes:
+                        dir_attribute = os.path.join(os.path.curdir, datatype, attribute)
+
+                        self.__update_file(data[attribute], dir_attribute, data["commit"], filename)
+                    self.__status = 1
+                    break
+
+        else:
+            self.__status = 0
+
+        response = {
+            "status": self.__status  # if successful otherwise 0
+        }
+
+        return response
 
     '''
     def list_repo(self,datatype, repo, sha):
