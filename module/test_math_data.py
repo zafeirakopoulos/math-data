0d0d46d840a0185a200694eaa30bccb380c5bd26
yilmazedis <ylmazedis@gmail.com>
2018-07-17 13:09:52 +0000

    added retrieve_instance

--- module/test_math_data.py
+++ module/test_math_data.py
@@ -4,7 +4,7 @@ from math_data import math_data
 data = math_data()
 
 r = {
-    "datatype": "graph",
+    "datatype": "polinom",
     "index": "something...",
     "raw": "something...",
     "features": "something...",
@@ -23,14 +23,47 @@ r = {
 
 
 r = {
-    "datatype": "graph",
-    "sha": "307fc2e45f95d123cb8a04ae343123ba11e064c9"
+    "datatype": "polinom",
+    "sha": "1b729f1219072705f1209820af80d525d60c3f42"
 }
 
 response = data.remove_instance(r)
 print(response)
 
+r = {
+    "datatype": "graph",
+    "sha": "e2b6af3d97a7f55cf9bd1b54a70614ca7598b04d",
+    "index": "deneme",
+    "raw": "something...",
+    "features": "something...",
+    "semantics": "something...",
+    "context":  {"edge": {"1": 33, "2": 5555},
+                 "vertex": [{"first": 4, "second": 4},
+                            {"first": 3, "second": 55}]},
+    "typeset": "something...",
+    "commit": "update_repo"
+}
 
+#response = data.update_instance(r)
+#print(response)
 
+r = {
+    "datatype": "graph",
+    "sha": "5ac30052d1a800b2d03bf8b841366b1652f470ee",
 
+}
 
+#response = data.retrieve_instance(r)
+#print(response)
+
+'''
+import git
+repo = git.Repo('/Users/yilmaz/PycharmProjects/miniserver/templates/graph/index')
+commits = repo.iter_commits()
+#commits.reverse()
+
+print(commits.hexsha)
+print(commits.message)
+print(commits.stats.total)
+print(commits.stats.files)
+'''
