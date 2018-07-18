59ff2067e45c44cd8192607393fb32e9356eea88
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2018-07-18 14:40:46 +0300

    Initialize MDB. TODO: The index file is not really committed

--- mathdatabase/manage.py
+++ mathdatabase/manage.py
@@ -1,13 +1,111 @@
 import os
 from git import Repo
+import hashlib
 
 class MDB:
-    __sha_list = []
+    __attributes = ("raw", "semantics", "typeset", "context", "features")
+    __repos = {}
+    ############################################################################
+    ############################################################################
+    ###  Functions to initialize and delete database
+    ############################################################################
+    ############################################################################
+    def __init__(self, basedir, datatype,debug=False):
+        datatypepath = os.path.join(basedir,datatype)
+        # Check if the folder for the datatype already exists
+        for file in os.listdir(basedir):
+            datatype_exists = False
+            if file == datatype:
+                datatype_exists = True
+                # The datatype folder exists
+                is_git = False
+                for file2 in os.listdir(datatypepath):
+                    # if it is a git repo, just return it
+                    if file2 == ".git":
+                        if debug: print("Folder exists and is git initialized")
+                        self.__repo = Repo(datatypepath).git
+                        self.__index = Repo(datatypepath).index
+                        if debug: print("Datatype exists and repository is initialized")
+                        is_git = True
+                        break
+                if not is_git:
+                    # If the folder is not a git repo, then initialize it
+                    if debug: print("Folder exists but not git initialized")
+                    self.__repo = Repo.init(datatypepath).git
+                    self.__index = Repo(datatypepath).index
+                    if debug: print("Initialized datatype: " + datatype)
+                break
+        # If the folder does not exist
+        if not datatype_exists:
+            if debug: print("Folder does not exist")
+            os.makedirs(datatypepath)
+            self.__repo = Repo.init(datatypepath).git
+            self.__index = Repo(datatypepath).index
+            if debug: print("Created folder and initialized datatype: " + datatype)
 
-    def __init__(self, name):
-        print "Initialized"
+        # Now we have to check or create the repos.
+
+        # We want to ingore these folders because they are repos themselves and
+        # they are tracked separately
+        #First check the .gitignore for the folders
+        hasher = hashlib.md5()
+        if os.path.exists(os.path.join(datatypepath,".gitignore")):
+            with open(os.path.join(datatypepath,".gitignore"), 'rb') as afile:
+                buf = afile.read()
+                hasher.update(buf)
+            if hasher.hexdigest()!="8f0b9d7f9079e1a2433447b16731cf86":
+                raise Exception(".gitignore not ok")
+        else:
+            # TODO Create the .gitignore
+            if debug: print(".gitignore does not exist. I will create it")
+            file = open(os.path.join(datatypepath,".gitignore"),"w")
+            for attribute in self.__attributes:
+                file.write(attribute)
+            file.close()
+            self.__repo.add(os.path.join(datatypepath,".gitignore"))
+            self.__index.commit("commit .gitignore for datatype " + datatype)
+
+
+        if not os.path.exists(os.path.join(datatypepath,"index")):
+            open(os.path.join(datatypepath,"index"), 'a').close()
+            self.__repo.add(os.path.join(datatypepath,"index"))
+            self.__index.commit("commit index for datatype " + datatype)
+
+
+        # Now check the 5 repos
+        for attribute in self.__attributes:
+            attribute_path = os.path.join(datatypepath,attribute)
+            # Check if the folder for the datatype already exists
+            for file in os.listdir(datatypepath):
+                attribute_exists = False
+                if file == attribute:
+                    attribute_exists = True
+                    # The datatype folder exists
+                    is_git = False
+                    for file2 in os.listdir(attribute_path):
+                        # if it is a git repo, just return it
+                        if file2 == ".git":
+                            if debug: print("Folder exists and is git initialized")
+                            self.__repos[attribute] = Repo(attribute_path).git
+                            if debug: print("Attribute exists and repository is initialized")
+                            is_git = True
+                            break
+                    if not is_git:
+                        # If the folder is not a git repo, then initialize it
+                        if debug: print("Folder exists but not git initialized")
+                        self.__repos[attribute] = Repo.init(attribute_path).git
+                        if debug: print("Initialized attribute: " + attribute)
+                    break
+            # If the folder does not exist
+            if not attribute_exists:
+                if debug: print("Folder does not exist")
+                os.makedirs(attribute_path)
+                self.__repos[attribute] = Repo.init(attribute_path).git
+                if debug: print("Created folder and initialized attribute: " + attribute)
+
+        if debug: print(self.__repos)
         return None
-         
+
     def __get_sha(self, path):
 
         master = Repo(path).head.reference
