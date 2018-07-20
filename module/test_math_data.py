8df2a73dcbe2f575f8d70c6e334acabaf0e06f01
yilmazedis <ylmazedis@gmail.com>
2018-07-20 12:12:37 +0000

    design changed and add sample usage.

--- module/test_math_data.py
+++ module/test_math_data.py
@@ -1,69 +1,131 @@
 from math_data import math_data
+import os
+import shutil
 
 
-data = math_data()
+def test_add_instance():
 
-r = {
-    "datatype": "polinom",
-    "index": "something...",
-    "raw": "something...",
-    "features": "something...",
-    "semantics": "something...",
-    "context":  {"edge": {"1": 1, "2": 2},
-                 "vertex": [{"first": 2, "second": 1},
-                            {"first": 23, "second": 55}]},
-    "typeset": "something...",
-    "commit": "something..."
-}
+    r = {
+        "datatype": "graph",
+        "index": "yeni...",
+        "raw": "something...",
+        "features": "something...",
+        "semantics": "something...",
+        "context":  {"edge": {"1": 1, "2": 2},
+                     "vertex": [{"first": 2, "second": 1},
+                                {"first": 23, "second": 55}]},
+        "typeset": "something...",
+        "commit": "something..."
+    }
 
-#firstly add some file after and store its index sha, And you can remove after that
-#response = data.add_instance(r)
-#print(response)
+    response = data.add_instance(r)
+    print(response)
+    return response
 
 
+def test_remove_instance(sha):
 
-r = {
-    "datatype": "polinom",
-    "sha": "1b729f1219072705f1209820af80d525d60c3f42"
-}
+    r = {
+        "datatype": "graph",
+        "sha": sha
+    }
 
-response = data.remove_instance(r)
-print(response)
+    response = data.remove_instance(r)
+    print(response)
+    return response
 
-r = {
-    "datatype": "graph",
-    "sha": "e2b6af3d97a7f55cf9bd1b54a70614ca7598b04d",
-    "index": "deneme",
-    "raw": "something...",
-    "features": "something...",
-    "semantics": "something...",
-    "context":  {"edge": {"1": 33, "2": 5555},
-                 "vertex": [{"first": 4, "second": 4},
-                            {"first": 3, "second": 55}]},
-    "typeset": "something...",
-    "commit": "update_repo"
-}
 
-#response = data.update_instance(r)
-#print(response)
+def test_update_instance(sha):
 
-r = {
-    "datatype": "graph",
-    "sha": "5ac30052d1a800b2d03bf8b841366b1652f470ee",
+    r = {
+        "datatype": "graph",
+        "sha": sha,
+        "index": "second test",
+        "raw": "something...",
+        "features": "safdfsdg",
+        "semantics": "something...",
+        "context":  {"edge": {"1": 100, "2": 2000},
+                     "vertex": [{"first": 4, "second": 4},
+                                {"first": 3, "second": 55}]},
+        "typeset": "something...",
+        "commit": "update_repo"
+    }
 
-}
+    response = data.update_instance(r)
+    print(response)
 
-#response = data.retrieve_instance(r)
-#print(response)
 
-'''
-import git
-repo = git.Repo('/Users/yilmaz/PycharmProjects/miniserver/templates/graph/index')
-commits = repo.iter_commits()
-#commits.reverse()
+def test_retrieve_instance(sha):
 
-print(commits.hexsha)
-print(commits.message)
-print(commits.stats.total)
-print(commits.stats.files)
-'''
+    r = {
+        "datatype": "graph",
+        "sha": sha,
+
+    }
+
+    response = data.retrieve_instance(r)
+    print(response)
+    return response
+
+
+if __name__ == "__main__":
+
+    datatypes = ["graph"]
+    path = "."
+
+    # preparation to sample
+    # removing old data types
+    for datatype in datatypes:
+        dir_datatype = os.path.join(path, datatype)
+
+        if os.path.exists(dir_datatype):
+            shutil.rmtree(dir_datatype, ignore_errors=True)
+            os.remove(os.path.join(path, "log.txt"))
+
+    data = math_data(datatypes=datatypes, path=path)
+
+    print("------  OPERATIONS  ------")
+    print("------  1. add      ------")
+    print("------  2. remove   ------")
+    print("------  3. update   ------")
+    print("------  4. retrieve ------")
+    print("------ -1. exit     ------")
+
+    operation = input("Which operation do you want : ")
+
+    while operation != "-1":
+        while not operation.isdigit():
+            print("Please enter a digit which are represented as table!")
+            operation = input("Which operation do you want : ")
+
+        if operation == "1":
+            # add operation
+            test_add_instance()
+
+        elif operation == "2":
+
+            print("Before remove operation we add a new data type")
+            sha = test_add_instance()
+
+            # remove operation
+            test_remove_instance(sha["sha"])
+        elif operation == "3":
+
+            print("Before update operation we add a new data type")
+            sha = test_add_instance()
+
+            # update operation
+            test_update_instance(sha["sha"])
+        elif operation == "4":
+
+            print("Before retrieve operation we add a new data type")
+            sha = test_add_instance()
+
+            # operation operation
+            test_retrieve_instance(sha["sha"])
+        else:
+            print(operation, "is not supported!")
+
+        operation = input("Which operation do you want : ")
+
+    print("Exited from the test!")
