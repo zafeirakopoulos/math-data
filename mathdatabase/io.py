dcb7c038c9eff5519bbbaa30bbee2173a611e9c5
yilmazedis <ylmazedis@gmail.com>
2018-07-24 07:58:21 +0000

    first yilmaz io.py script

--- mathdatabase/io.py
+++ mathdatabase/io.py
@@ -1,16 +1,334 @@
+from threading import Lock
 import os
+import json
 from git import Repo
 
-def add_instance(mdb, datatype, instance):
-    """Add an instance in the mathdatabase.
+mutex = Lock()
+
+
+def __add_helper(mdb, data):
+    """ Private Helper Function.
+    Go through target path and creates new data type
+    :param data: incoming json file.
+    :param sha_list: sha list of all attribute such as context
+    :return: none
     """
 
-def remove_instance(mdb,  instance):
-    """Remove an instance from the mathdatabase.
+    datatype = data["datatype"]
+
+    if os.path.exists(datatype):
+
+        # update static log to update log.txt
+        mdb.get_file_number()[datatype] += 1
+        mdb.get_file_number()["remaining-" + datatype] += 1
+
+        filename = "file" + str(mdb.get_file_number()[datatype]) + ".txt"
+
+        for attribute in mdb.get_attributes():
+
+            dir_attribute = os.path.join(mdb.get_current_path(), datatype, attribute)
+            file_path = os.path.join(dir_attribute, filename)
+
+            if os.path.exists(dir_attribute):
+
+                # create new file and add to repo
+                with open(file_path, 'w') as outfile:
+                    json.dump(data[attribute], outfile)
+
+                # open repo
+                repo = Repo.init(dir_attribute).git
+                index = Repo(dir_attribute).index
+
+                # add file to repo
+                repo.add(filename)
+                index.commit(data["commit"])
+                # --------------------------------
+
+        # update log file according to data type file_number
+        dir_log = os.path.join(mdb.get_current_path(), "log.txt")
+        with open(dir_log, 'w') as outfile:
+            json.dump(mdb.get_file_number(), outfile)
+
+        # return index sha key
+        return mdb.get_sha(os.path.join(mdb.get_current_path(), datatype, "index"), filename)
+
+    else:
+        pass
+
+
+def add_instance(mdb, data):
+    """
+    It is dependent to name of data type.
+    if new data type required to add new file structure is created.
+    Otherwise, add just a new file.
+
+    input example;
+        r = {
+            "datatype": "graph",   # may be polynomial
+            "index": "index example",
+            "raw": "raw example",
+            "features": "features example",
+            "semantics": "semantics example",
+            "context":  {"edge": {"1": 100, "2": 2000},
+                         "vertex": [{"first": 4, "second": 4},
+                                    {"first": 3, "second": 55}]},
+            "typeset": "typeset example",
+            "commit": "update_repo"
+        }
+    return example;
+
+        {sha : a63813d76910623f2b92ca7343682fe9ee2230a1 , 'status': 1}
+        if removal is unsuccessful;
+        {'status': 0} # this line will be rewrite
+
+    :param data: incoming json file.
+    :return: sha key of index of datatype.
     """
 
-def retrieve_instance(mdb,  key):
-    """Retrieve an instance from the mathdatabase.
+    mutex.acquire()
+    try:
+        index_sha = __add_helper(mdb, data)
+    finally:
+        mutex.release()
+
+    status = 1  # will be fix
+
+    response = {
+        "sha": index_sha,    # index represent all of datatype to perform on it.
+        "status": status                            # if successful otherwise 0
+    }
+
+    return response
+
+
+def remove_instance(mdb, data):
     """
-    instance = {}
-    return instance
+    It is dependent to name of data type and its sha key.
+    An example as fallow;
+
+    input example;
+
+        r = {
+            "datatype": "graph",
+            "sha": "a9f870b98077b86f4cff2afbb90c3255c8f9a923"
+        }
+
+        other example
+
+        r = {
+            "datatype": "polynomial",
+            "sha": "12b8a0b98077b86f4cff2afbb90c3255c8f9affc"
+        }
+
+    return example;
+
+        {'status': 1}
+
+        if removal is unsuccessful;
+
+        {'status': 0}
+
+    :param data: incoming json file.
+    :return: control status
+    """
+    status = 0
+    datatype = data["datatype"]
+
+    if os.path.exists(datatype):
+
+        input_sha = data["sha"]
+
+        dir_index = os.path.join(mdb.get_current_path(), datatype, "index")
+
+        for filename in os.listdir(dir_index):
+
+            # we ignore .git file
+            if filename == ".git":
+                continue
+
+            if input_sha == mdb.get_sha(dir_index, filename):
+
+                for attribute in mdb.get_attributes():
+
+                    path = os.path.join(mdb.get_current_path(), datatype, attribute)
+
+                    index = Repo(path).index
+                    repo = Repo(path).git
+                    repo.rm(filename)
+
+                    index.commit("deleted repo")
+                status = 1
+
+                # decrease log file data type and write to log again
+                mdb.get_file_number()["remaining-" + datatype] -= 1
+                with open(os.path.join(mdb.get_current_path(), "log.txt"), 'w') as outfile:
+                    json.dump(mdb.get_file_number(), outfile)
+
+                break
+
+    response = {
+        "status": status  # if successful otherwise 0
+    }
+
+    return response
+
+
+def retrieve_instance(mdb, data):
+
+    """ Fetches an instance which match with "input" sha key
+
+    input example;
+
+        r = {
+            "datatype": "graph",
+            "sha": "18e8b2047eeefe9d45fa01a2f15b26bb62a3c471"
+        }
+
+        other example
+
+        r = {
+            "datatype": "polynomial",
+            "sha": "21fa767a68101f4b7e75ffe50001954d0ee37a74"
+        }
+
+    return example;
+
+        r = {
+            "datatype": "graph",   # may be polynomial
+            "index": "index example",
+            "raw": "raw example",
+            "features": "features example",
+            "semantics": "semantics example",
+            "context":  {"edge": {"1": 100, "2": 2000},
+                         "vertex": [{"first": 4, "second": 4},
+                                    {"first": 3, "second": 55}]},
+            "typeset": "typeset example"
+        }
+
+    :param data: incoming json file.
+    :return: desired instance
+    """
+
+    datatype = data["datatype"]
+
+    response = {"datatype": datatype}
+
+    status = -1
+
+    if not os.path.exists(datatype):
+        status = 0
+
+    else:
+        input_sha = data["sha"]
+
+        dir_index = os.path.join(mdb.get_current_path(), datatype, "index")
+
+        for filename in os.listdir(dir_index):
+
+            if filename == ".git":
+                continue
+
+            if input_sha == mdb.get_sha(dir_index, filename):
+                for attribute in mdb.get_attributes():
+                    file_path = os.path.join(mdb.get_current_path(), datatype, attribute, filename)
+
+                    with open(file_path) as json_file:
+                        response[attribute] = json.load(json_file)
+
+                status = 1
+                break
+
+    response["status"] = status
+
+    return response
+
+
+def __update_file(content, path, commit_message, filename):
+
+    """Private Helper Function.
+    Write new data to target file. Add repository with new commit message
+    :param content: New content to edit target file
+    :param path: repository path
+    :param commit_message:
+    :param filename:
+    :return: none
+    """
+    repo = Repo(path).git
+    index = Repo(path).index
+
+    # generate path
+    file_path = os.path.join(path, filename)
+
+    # write target file
+    with open(file_path, 'w') as outfile:
+        json.dump(content, outfile)
+
+    # add repo
+    repo.add(filename)
+    index.commit(commit_message)
+    
+    
+def update_instance(mdb, data):
+    """
+    update instance which match with "input" sha key
+
+    input example;
+
+        r = {
+            "datatype": "graph",   # may be polynomial
+            "sha": "12b8a0b98077b86f4cff2afbb90c3255c8f9affc",
+            "index": "index example",
+            "raw": "raw example",
+            "features": "features example",
+            "semantics": "semantics example",
+            "context":  {"edge": {"1": 100, "2": 2000},
+                         "vertex": [{"first": 4, "second": 4},
+                                    {"first": 3, "second": 55}]},
+            "typeset": "typeset example",
+            "commit": "update_repo"
+        }
+
+
+    return example;
+
+        {'status': 1}
+
+        if removal is unsuccessful;
+
+        {'status': 0}
+
+    :param data: incoming json file.
+    :return:
+    """
+
+    datatype = data["datatype"]
+
+    status = 0
+
+    if not os.path.exists(datatype):
+        status = 0
+
+    else:
+        input_sha = data["sha"]
+
+        dir_index = os.path.join(mdb.get_current_path(), datatype, "index")
+
+        for filename in os.listdir(dir_index):
+
+            if filename == ".git":
+                continue
+
+            if input_sha == mdb.get_sha(dir_index, filename):
+                for attribute in mdb.get_attributes():
+                    dir_attribute = os.path.join(mdb.get_current_path(), datatype, attribute)
+
+                    __update_file(data[attribute], dir_attribute, data["commit"], filename)
+                status = 1
+                break
+
+    response = {
+        "status": status  # if successful otherwise 0
+    }
+
+    return response
+
