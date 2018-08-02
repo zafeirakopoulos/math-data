b09cb467082ebf93beb641596af16d6af0159d27
adem kaya <ademkayafb@hotmail.com>
2018-08-02 13:19:12 +0000

    Update app.py

--- mdb/ui/app.py
+++ mdb/ui/app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request, jsonify
-#from database import manage,io
-#import os
+from database import manage,io
+import os
 app = Flask(__name__)
 
 
@@ -16,12 +16,22 @@ jsonType = {
                      }
 }
 
-
+r = {
+        "raw": "something-",
+        "features": "something-",
+        "semantics": "something-",
+        "context":  {"edge": {"1": 99, "2": 3},
+                     "vertex": [{"first": 1, "second": 2},
+                                {"first": 2, "second": 3}]},
+        "typeset": "something-",
+        "commit": "commit-message"
+    }
 @app.route("/",methods= ["GET" , "POST"])
 def start():
-    # basedir = os.path.join(os.getcwd(), "data")
-    # data = manage.mdb(basedir=basedir)
-    # print(io.add_instance(data,products))
+    if request.method == "POST":
+        basedir = os.path.join(os.getcwd(), "data")
+        data = manage.mdb(basedir=basedir)
+        print(io.add_instance(data,r))
     return render_template("mathData.html")
 
 
@@ -29,11 +39,12 @@ def start():
 @app.route('/datatypes',methods= ["GET" , "POST"])
 def get_products():
 
-        data1 = request.form.get("jsonFile")
-        print("form", *request.form.items())
-        print(data1)
+    data1 = request.form.get("jsonFile")
+    print("form", *request.form.items())
+    print(data1)
 
-        return jsonify({'jsonType': jsonType})
+    return jsonify({'jsonType': jsonType})
+
 
 if __name__ == '__main__':
    app.run(debug = True)
