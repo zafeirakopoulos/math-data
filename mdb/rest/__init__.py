f2d00815ef7aca5980bc4c662e27f0175563a022
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2018-08-07 00:35:29 +0300

    Fix bugs

--- mdb/rest/__init__.py
+++ mdb/rest/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from rest.data import *
-from rest.benchmarks import *
+from mdb.rest.data import *
+from mdb.rest.benchmarks import *
 
 app = Flask(__name__)
 app.register_blueprint(data_app, url_prefix='/data')
 app.register_blueprint(benchmarks_app, url_prefix='/benchmarks')
 
-# The app should start in a separate thread 
+# The app should start in a separate thread
 # This does not work in debug mode.
 #thread.start_new_thread(flaskThread,())
