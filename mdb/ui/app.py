b6fa62e440417d61ecaa0841a2233fba5f99fc3a
adem kaya <ademkayafb@hotmail.com>
2018-08-06 10:49:14 +0000

    Update app.py

--- mdb/ui/app.py
+++ mdb/ui/app.py
@@ -1,53 +1,127 @@
-from flask import Flask,render_template,request, jsonify
-from database import manage,io
+from flask import Flask,request, json
+from mdb.database import manage, io, search
 import os
 app = Flask(__name__)
 
+@app.route('/add_instance', methods=["POST"])
+def add_instance():
 
-jsonType = {
+    basedir = os.path.join(os.getcwd(), "data")
+    global data
+    data = manage.mdb(basedir=basedir)
 
-    "raw": {"dense": {"structure": "matrix"},
-            "sparse": {"structure": "list"}
-            },
+    instance = request.json
 
-    "typeset": {},
+    response = io.add_instance(data, instance)
 
-    "features": {"directed": {"structure": "boolean"}
-                     }
-}
+    js = json.dumps(response)
 
-r = {
-        "raw": "something-",
-        "features": "something-",
-        "semantics": "something-",
-        "context":  {"edge": {"1": 99, "2": 3},
-                     "vertex": [{"first": 1, "second": 2},
-                                {"first": 2, "second": 3}]},
-        "typeset": "something-",
-        "commit": "commit-message"
-    }
-@app.route("/",methods= ["GET" , "POST"])
-def start():
-    if request.method == "POST":
-        basedir = os.path.join(os.getcwd(), "data")
-        data = manage.mdb(basedir=basedir)
-        print(io.add_instance(data,r))
-    return render_template("mathData.html")
+    return js
 
 
+@app.route('/filter', methods=["POST"])
+def filter():
 
-@app.route('/datatypes',methods= ["GET" , "POST"])
-def get_products():
+    basedir = os.path.join(os.getcwd(), "data")
+    global data
+    data = manage.mdb(basedir=basedir)
 
-    data1 = request.form.get("jsonFile")
-    print("form", *request.form.items())
-    print(data1)
+    instance = request.json
 
-    return jsonify({'jsonType': jsonType})
+    # filter method are not written yet
+    response = search.filter(data, instance)
+
+    js = json.dumps(response)
+
+    return js
+@app.route('/dataset', methods=["POST"])
+def dataset():
+    basedir = os.path.join(os.getcwd(), "data")
+    global data
+    data = manage.mdb(basedir=basedir)
+    instance = request.json
+    # dataset method are not written yet
+    response = search.search(data, instance)
+    js = json.dumps(response)
+    return js
+
+@app.route('/datatypes', methods=["GET"])
+def datatypes():
+    # it is an example
+    data = [
+        {"datatype": "graph"},
+        {"datatype": "polynomial"},
+        {"datatype": "list"}
+    ]
+    js = json.dumps(data)
+    return js
+
+@app.route('/datasets', methods=["GET"])
+def datasets():
+
+    # it is an example
+    data = [
+        {"vertex": "20"},
+        {"edge": "10"},
+    ]
+
+    js = json.dumps(data)
+
+    return js
+
+
+@app.route('/instances', methods=["GET"])
+def instances():
+
+    # it is an example
+    instance = [
+        {
+            "raw": "something-",
+            "features": "something-",
+            "semantics": "something-",
+            "context": {"edge": {"1": 99, "2": 3},
+                        "vertex": [{"first": 1, "second": 2},
+                                   {"first": 2, "second": 3}]},
+            "typeset": "something-",
+            "commit": "commit-message"
+        },
+
+        {
+            "raw": "1",
+            "features": "2-",
+            "semantics": "2-",
+            "context": {"edge": {"1": 99, "2": 3},
+                        "vertex": [{"first": 1, "second": 2},
+                                   {"first": 2, "second": 3}]},
+            "typeset": "3-",
+            "commit": "commit-message"
+        }
+    ]
+
+    js = json.dumps(instance)
+
+    return js
+
+
+@app.route('/instance', methods=["POST"])
+def instance():
+
+    # it is an example
+
+    basedir = os.path.join(os.getcwd(), "data")
+    global data
+    data = manage.mdb(basedir=basedir)
+    instance = request.json
+
+    response = io.retrieve_instance(data, instance)
+
+    js = json.dumps(response)
+
+    return js
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(debug=True)
 
 
 
