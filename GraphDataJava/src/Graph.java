05bd253e7a082a961d4f46b3164f307403b9adbb
gizemsungu <gizemsungu@gtu.edu.tr>
2018-09-03 11:14:02 +0300

    pyhton functions are improved

--- GraphDataJava/src/Graph.java
+++ GraphDataJava/src/Graph.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Graph extends Element {
-    private ArrayList<Node> nodes;
-    private ArrayList<Edge> edges;
-    private static int nodeId;
-    private static int edgeId;
+    private ArrayList<Node> nodes;//json
+    private ArrayList<Edge> edges;//json
+    private static int nodeId;//functional
+    private static int edgeId;//functional
 
     public Graph(int id) {
         super(id);
@@ -19,11 +19,34 @@ public class Graph extends Element {
         nodeId++;
     }
 
-    public void addEdge(Node startNode, Node endNode, boolean directed){
-        if (this.nodes.contains(startNode) && this.nodes.contains(endNode)) {
+    public void addNode(int nodeSize){
+        for (int i = 0; i < nodeSize ; i++) {
+            addNode();
+        }
+    }
+
+    public String addEdge(int startNodeId, int endNodeId, boolean directed){
+        int nodeSize = this.nodes.size();
+        if (startNodeId < nodeSize && endNodeId < nodeSize) {
+            Node startNode = getNodeById(startNodeId);
+            Node endNode = getNodeById(endNodeId);
+            if (directed){
+                if(searchIfExistDirected(startNode, endNode))
+                    return "The directed edge has already existed.";
+            }
+            else{
+                if(searchIfExistUndirected(startNode, endNode))
+                    return "The undirected edge has already existed.";
+            }
+
             Edge newEdge = new Edge(edgeId, startNode, endNode, directed);
             this.edges.add(newEdge);
             edgeId++;
+            return "The new edge between " + startNodeId + " and " + endNodeId + " is added with " + newEdge.getId() + " id.";
+
+        }
+        else {
+            return "Start node and/or end node do/does not contain in the graph. Please try another node(s).";
         }
     }
 
@@ -33,6 +56,33 @@ public class Graph extends Element {
 
     public ArrayList<Edge> getEdges() {
         return edges;
+    }
+
+    public Node getNodeById(int id){
+        return this.nodes.get(id);
+    }
+
+    public boolean searchIfExistDirected(Node startNode, Node endNode){
+        for (Edge edge: this.edges) {
+            if (edge.getStartNode().equals(startNode) && edge.getEndNode().equals(endNode)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean searchIfExistUndirected(Node startNode, Node endNode){
+        for (Edge edge: this.edges) {
+            if (edge.getStartNode().equals(startNode) && edge.getEndNode().equals(endNode)){
+                if (!edge.isDirected())
+                    return true;
+            }
+            else if(edge.getStartNode().equals(endNode) && edge.getEndNode().equals(startNode)){
+                if(!edge.isDirected())
+                    return true;
+            }
+        }
+        return false;
     }
 
 }
