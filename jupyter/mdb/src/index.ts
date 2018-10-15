065ee1f5d05c3c146ea1405618dcdef1e43f0ed3
M. Oguzhan Ataman <moguzhanataman@gmail.com>
2018-10-15 19:45:09 +0300

    Formatting

--- jupyter/mdb/src/index.ts
+++ jupyter/mdb/src/index.ts
@@ -1,105 +1,84 @@
-
 // Copyright (c) MathDataBench Development Team.
 
+import {
+  JupyterLab,
+  JupyterLabPlugin,
+  ILayoutRestorer
+} from "@jupyterlab/application";
 
+import "../style/index.css";
+
+import { ICommandPalette, InstanceTracker } from "@jupyterlab/apputils";
+
+import { Widget } from "@phosphor/widgets";
 
 import {
-  JupyterLab, JupyterLabPlugin, ILayoutRestorer
-} from '@jupyterlab/application';
+  JSONExt //Token
+} from "@phosphor/coreutils";
 
-import '../style/index.css';
-
-import {
-ICommandPalette, InstanceTracker
-} from '@jupyterlab/apputils';
-
-import {
-Widget
-} from '@phosphor/widgets';
-
-import {
-JSONExt, //Token
-} from '@phosphor/coreutils';
-
-
-import {
-MathDataWidget, MathDataTOC
-} from './mdb';
-
-
-
-
-
+import { MathDataWidget, MathDataTOC } from "./mdb";
 
 /**
  * Initialization data for the mdb extension.
  */
 const extension: JupyterLabPlugin<void> = {
-  id: 'mdb',
+  id: "mdb",
   autoStart: true,
-  requires: [ICommandPalette,
-             ILayoutRestorer
-            ],
-  activate: (   app: JupyterLab,
-                palette: ICommandPalette,
-                restorer: ILayoutRestorer
-            ) => {
-                console.log('JupyterLab extension mdb is activated!');
+  requires: [ICommandPalette, ILayoutRestorer],
+  activate: (
+    app: JupyterLab,
+    palette: ICommandPalette,
+    restorer: ILayoutRestorer
+  ) => {
+    console.log("JupyterLab extension mdb is activated!");
 
-                const { commands } = app;
+    const { commands } = app;
 
-                let widget: MathDataWidget;
-                widget  = new MathDataWidget();
+    let widget: MathDataWidget;
 
-                console.log(widget.id)
-                // Add an application command
-                const command: string = 'mdb:open';
-                commands.addCommand(command, {
-                    label: 'MathDataBench',
-                    execute: () => {
-                        if (!widget) {
-                            // Create a new widget if one does not exist
-                            widget.update();
-                        }
+    // Add an application command
+    const command: string = "mdb:open";
+    commands.addCommand(command, {
+      label: "MathDataBench",
+      execute: () => {
+        if (!widget) {
+          // Create a new widget if one does not exist
+          widget = new MathDataWidget();
+          widget.update();
+        }
 
-                        if (!tracker.has(widget)) {
-                        // Track the state of the widget for later restoration
-                        tracker.add(widget);
-                        }
+        if (!tracker.has(widget)) {
+          // Track the state of the widget for later restoration
+          tracker.add(widget);
+        }
 
-                        if (!widget.isAttached) {
-                            // Attach the widget to the main work area if it's not there
-                            app.shell.addToMainArea(widget);
-                        } else {
-                            // Refresh the comic in the widget
-                            widget.update();
-                        }
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.addToMainArea(widget);
+        } else {
+          // Refresh the comic in the widget
+          widget.update();
+        }
 
-                        // Activate the widget
-                        app.shell.activateById(widget.id);
-                    }
-                });
-                palette.addItem({command, category: 'MathDataBench'});
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+    palette.addItem({ command, category: "MathDataBench" });
 
+    // Track and restore the widget state
+    let tracker = new InstanceTracker<Widget>({ namespace: "MathDataBench" });
+    restorer.restore(tracker, {
+      command,
+      args: () => JSONExt.emptyObject,
+      name: () => "MathDataBench"
+    });
 
-                // Track and restore the widget state
-                let tracker  = new InstanceTracker<Widget>({ namespace: 'MathDataBench' });
-                restorer.restore(tracker, {
-                command,
-                args: () => JSONExt.emptyObject,
-                name: () => 'MathDataBench'
-                });
+    let tocwidget: MathDataTOC;
+    tocwidget = new MathDataTOC();
 
-
-                let tocwidget: MathDataTOC;
-                tocwidget  = new MathDataTOC();
-
-
-                app.shell.addToLeftArea(tocwidget, { rank: 700 });
-
-            }
-    };
-
-
+    app.shell.addToLeftArea(tocwidget, { rank: 700 });
+  }
+};
 
 export default extension;
