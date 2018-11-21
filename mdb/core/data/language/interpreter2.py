c48af528445b810b1442e7f5b887fa18557a4c2e
gizemsungu <gizemsungu@gtu.edu.tr>
2018-11-21 16:48:48 +0300

    interpreter

--- mdb/core/data/language/interpreter2.py
+++ mdb/core/data/language/interpreter2.py
@@ -7,15 +7,15 @@ from jsonschema import validate
 new_path = sys.path[0]
 new_path = new_path[:-10] + "/language/"
 sys.path[0] = new_path
-path_def = new_path[:-23] + "/local/defs/"
+DEF_PATH = new_path[:-23] + "/local/defs/"
 print(path_def)
 new_path = new_path[:-23] + "/local/third_party/"
 print(new_path)
-PATH = new_path   #should be changed
+DATA_PATH = new_path   #should be changed
 
 
 def find_def_file(name):
-    with open(path_def+"def.index") as defIndex:
+    with open(DEF_PATH+"def.index") as defIndex:
         index = json.load(defIndex)
 
         if name not in index:
@@ -23,12 +23,12 @@ def find_def_file(name):
         else:
             # TODO Find correct revision of the file
             filename = index[name][0]
-            with open(path_def+filename+".def") as defFile:
+            with open(DEF_PATH+filename+".def") as defFile:
                 definition = json.load(defFile)
                 return definition
 
 def validate(data):
-	with open(new_path+data+".json") as dataFile:
+	with open(DATA_PATH+data+".json") as dataFile:
 		data_file = json.load(dataFile)
 		def_file = find_def_file(data_file["name"])
                 pprint.pprint(def_file)
