35fcc130faf4ee4af98c14af87a61ad998f0f98d
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2019-01-31 20:48:17 +0300

    A new beginning

---
+++ new/md_def.py
@@ -0,0 +1,8 @@
+import os
+
+mdb_path = os.getcwd()
+
+mdb_name = "live"
+mdb_definitions_folder = ['local','definitions']
+
+mdb_definition = '{"paths": ["raw", "options"]}'
---
+++ new/mdb/data/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask, Blueprint
+from mdb.db import active_db
+data_app = Blueprint('data', __name__, template_folder='templates/data')
+data_app.active_mdb = active_db
+
+import mdb.data.views
