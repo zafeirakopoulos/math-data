122a4172dfaea469fbff0d35c237a71b7a464435
zaf <zafeirakopoulos@gmail.com>
2020-05-19 17:01:52 +0300

    Chang enavbar

--- mdb/views/home/views.py
+++ mdb/views/home/views.py
@@ -7,7 +7,7 @@ from mdb.core import create_response, serialize_list, logger
 def index():
     static_definition = '"MathData - A system for efficient representation of mathematical data and benchmarking of mathematical software and algorithms" is a TUBITAK funded project aiming to create a standard for representing mathematical data and testing mathematical software.\nThe duration of the project is 18 months and it started on October 2017.'
     stats = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    
+
     return render_template('index.html', definition=static_definition, stats=stats)
 
 @home_app.route('/contact', methods=['GET'])
@@ -36,3 +36,9 @@ def contact():
 def get_persons():
     persons = User.query.all()
     return create_response(data={"persons": serialize_list(persons)})
+
+
+@home_app.route('/documentation', methods=['GET'])
+def documentation():
+
+    return render_template('documentation.html')
