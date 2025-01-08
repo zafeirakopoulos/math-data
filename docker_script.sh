17db98685293f031f66b93c8d92e6c53e9416964
erayozkan7 <eray.ozkan15@gmail.com>
2025-01-08 23:45:54 +0300

    added comment for the run error after cloning

--- docker_script.sh
+++ docker_script.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+#convert the line endings CRLF to LF for error: 
+# app exited with code 1
+# app       | exec /app/docker_script.sh: no such file or directory
 
 python /app/manage.py runserver
